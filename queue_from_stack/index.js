1c7675069b3a7bdf675f858ecf17f552a12322d4
yagmurcetintas <yagmurcetin@gmail.com>
2020-10-21 09:16:02 -0400

    added solution to the queue_from_stack problem

--- queue_from_stack/index.js
+++ queue_from_stack/index.js
@@ -3,6 +3,36 @@
 
 const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.data = new Stack();
+    this.finalData = new Stack();
+  }
+  add(record) {
+    this.data.push(record);
+  }
+
+  remove() {
+    while (this.data.peek()) {
+      this.finalData.push(this.data.pop());
+    }
+    const record = this.finalData.pop();
+    while (this.finalData.peek()) {
+      this.data.push(this.finalData.pop());
+    }
+    return record;
+  }
+
+  peek() {
+    while (this.data.peek()) {
+      this.finalData.push(this.data.pop());
+    }
+    const record = this.finalData.peek();
+    while (this.finalData.peek()) {
+      this.data.push(this.finalData.pop());
+    }
+    return record;
+  }
+}
 
 module.exports = Queue;
